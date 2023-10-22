9ba94c8f8aa1a99d5cd4041bdbba0f644f6cc6b0
coder-rancho <coder.namanvyas@gmail.com>
2023-10-22 11:02:30 +0530

    Setup Recoil root component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
-import Head from 'next/head'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import RecoilContextProvider from './recoilContextProvider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,10 +18,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        < RecoilContextProvider >{children}</RecoilContextProvider>
+      </body>
     </html>
   )
 }
